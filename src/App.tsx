1ec41f405e5edbf59d90d0e82e48c9d774086aea
erntorii <erntorii@gmail.com>
2021-06-27 17:13:48 +0900

    add login/logout to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./App.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { selectUser, login, logout } from "./features/userSlice";
+import { auth } from "./firebase";
 
-function App() {
-  return <div></div>;
-}
+import Feed from "./components/Feed";
+import Auth from "./components/Auth";
+
+const App = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [dispatch]);
+
+  return <div>{user.uid ? <Feed /> : <Auth />}</div>;
+};
 
 export default App;
