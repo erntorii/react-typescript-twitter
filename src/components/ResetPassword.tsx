2a69071faf821058902032030159876f59968313
erntorii <erntorii@gmail.com>
2021-06-28 20:10:45 +0900

    fix ResetPassword layouts

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -1,9 +1,33 @@
 import React, { useState } from "react";
 import { auth } from "../firebase";
-import { Input, Button, Link } from "@material-ui/core";
+import {
+  makeStyles,
+  Container,
+  TextField,
+  Button,
+  Link,
+} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
 import { useHistory } from "react-router-dom";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(15),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 const ResetPassword = () => {
+  const classes = useStyles();
   const [resetEmail, setResetEmail] = useState("");
   const sendPasswordResetEmail = async () => {
     await auth
@@ -20,18 +44,40 @@ const ResetPassword = () => {
   const history = useHistory();
 
   return (
-    <div>
-      <h2>Reset Password</h2>
-      <Input
-        type="text"
-        value={resetEmail}
-        onChange={(e) => setResetEmail(e.target.value)}
-      />
-      <Button onClick={sendPasswordResetEmail}>Send</Button>
-      <Link onClick={() => history.goBack()} variant="body2">
-        Back
-      </Link>
-    </div>
+    <Container maxWidth="xs">
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Reset Password
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={resetEmail}
+            onChange={(e) => setResetEmail(e.target.value)}
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={sendPasswordResetEmail}
+          >
+            Send
+          </Button>
+          <Link onClick={() => history.goBack()} variant="body2">
+            Back
+          </Link>
+        </form>
+      </div>
+    </Container>
   );
 };
 
