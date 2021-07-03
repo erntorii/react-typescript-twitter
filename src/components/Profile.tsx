a040abe8e54bc5ead4ab32478e03cc00eae531ba
erntorii <erntorii@gmail.com>
2021-07-03 12:07:14 +0900

    fix layouts, add username set

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,6 +3,7 @@ import { auth, storage } from "../firebase";
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser, updateUserProfile } from "../features/userSlice";
 import { v4 as uuid } from "uuid";
+import { useHistory } from "react-router-dom";
 
 import {
   Avatar,
@@ -10,10 +11,38 @@ import {
   TextField,
   Button,
   Typography,
+  Box,
   Container,
+  makeStyles,
+  Link,
 } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  avatarSize: {
+    width: "120px",
+    height: "120px",
+  },
+  hiddenInput: {
+    textAlign: "center",
+    display: "none",
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 const Profile = () => {
+  const classes = useStyles();
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
   const [username, SetUsername] = useState(user.displayName);
@@ -48,30 +77,53 @@ const Profile = () => {
       })
     );
   };
+  const history = useHistory();
 
   return (
     <Container maxWidth="xs">
-      <Typography component="h1" variant="h5">
-        Update Profile
-      </Typography>
-      <IconButton>
-        <label>
-          <Avatar src={prevAvatarUrl} />
-          <input type="file" onChange={onChangeImageHandler} />
-        </label>
-      </IconButton>
-      <TextField
-        variant="outlined"
-        margin="normal"
-        fullWidth
-        label="Username"
-        name="username"
-        value={username}
-        onChange={(e) => SetUsername(e.target.value)}
-      />
-      <Button fullWidth color="primary" onClick={updateProfile}>
-        Save
-      </Button>
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Profile Settings
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Box textAlign="center">
+            <IconButton>
+              <label>
+                <Avatar className={classes.avatarSize} src={prevAvatarUrl} />
+                <input
+                  type="file"
+                  className={classes.hiddenInput}
+                  onChange={onChangeImageHandler}
+                />
+              </label>
+            </IconButton>
+          </Box>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            fullWidth
+            label="Username"
+            name="username"
+            value={username}
+            onChange={(e) => SetUsername(e.target.value)}
+          />
+          <Button
+            disabled={!username}
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={() =>
+              updateProfile().then(() => alert("Profile updated."))
+            }
+          >
+            Save
+          </Button>
+          <Link onClick={() => history.goBack()} variant="body2">
+            Back
+          </Link>
+        </form>
+      </div>
     </Container>
   );
 };
